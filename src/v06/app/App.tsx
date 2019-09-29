854b42eea1ef789fc9055541f56478e717a7dd38
David Waller <dwaller@apple.com>
2019-09-29 16:02:49 +0200

    fix thunk store bapp

--- src/v06/app/App.tsx
+++ src/v06/app/App.tsx
@@ -1,19 +1,21 @@
 import React, { FunctionComponent, useMemo } from 'react'
 
 import { Provider } from 'react-redux'
-import { makeStore } from '../redux'
+import { makeStore, AppState } from '../redux'
 
 import { Main } from './Main'
 
 import '@blueprintjs/core/lib/css/blueprint.css'
 import './App.css'
+import { AppAction } from '../redux'
+import { Store } from 'redux'
 
 type AppProps = { version: string }
 
 export const App: FunctionComponent<AppProps> = ({ version }) => {
   const store = useMemo(() => makeStore(), [])
   return (
-    <Provider store={store}>
+    <Provider store={store as Store<AppState, AppAction>}>
       <Main version={version} />
     </Provider>
   )
