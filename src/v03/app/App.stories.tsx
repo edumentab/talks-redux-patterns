cb664959f03c9a644c9017fda04bfbf6c458c5cc
David Waller <dwaller@apple.com>
2019-09-29 08:28:43 +0200

    fix

--- src/v03/app/App.stories.tsx
+++ src/v03/app/App.stories.tsx
@@ -4,5 +4,5 @@ import React from 'react'
 import { App } from './App'
 
 storiesOf('v03', module).add('Store test', () => {
-  return <App key="v02" version="v03-store" />
+  return <App key="v03" version="v03-store" />
 })
