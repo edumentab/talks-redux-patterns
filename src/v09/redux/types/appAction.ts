ebf8d0eb3e2331fb7f9cfd916a821b5ea4b52cf5
David Waller <dwaller@apple.com>
2019-09-30 11:18:10 +0200

    simplify appaction with guard

--- src/v09/redux/types/appAction.ts
+++ src/v09/redux/types/appAction.ts
@@ -1,26 +1,3 @@
-import {
-  LoadSetsErrorAction,
-  LoadSetsInitAction,
-  LoadSetsSuccessAction,
-  LoadThemesErrorAction,
-  LoadThemesInitAction,
-  LoadThemesSuccessAction
-} from '../slices/rebrickable/actions'
+import { Action } from '../lib/types/action'
 
-import { MakeGuessAction } from '../slices/guessingGame/actions'
-
-import {
-  SetCurrentSetAction,
-  SetCurrentThemeAction
-} from '../slices/ui/actions'
-
-export type AppAction =
-  | LoadSetsErrorAction
-  | LoadSetsSuccessAction
-  | LoadSetsInitAction
-  | LoadThemesErrorAction
-  | LoadThemesInitAction
-  | LoadThemesSuccessAction
-  | MakeGuessAction
-  | SetCurrentSetAction
-  | SetCurrentThemeAction
+export type AppAction = Action<string, any>
