1ff0a526dda36c26526b8a7a285d4a6d7cb21701
David Waller <dwaller@apple.com>
2019-10-01 17:22:10 +0200

    more links

--- .storybook/presentationPanel/presentationV99.jsx
+++ .storybook/presentationPanel/presentationV99.jsx
@@ -9,6 +9,8 @@ export const PresentationV99 = () => {
         source={`
 * [https://redux-patterns.netlify.com](https://redux-patterns.netlify.com)
 * [https://github.com/edumentab/talks-redux-patterns](https://github.com/edumentab/talks-redux-patterns)
+* [https://blog.krawaller.se/](https://blog.krawaller.se/)
+* [https://edument.se/](https://edument.se/)
 * [mailto:david@krawaller.se](david@krawaller.se)
 
 `}
