834bbd623914cdee24de249034b49e675a00815f
David Waller <dwaller@apple.com>
2019-11-21 19:14:31 +0100

    clever default expand

--- .refac/components/sourceTree.js
+++ .refac/components/sourceTree.js
@@ -6,7 +6,12 @@ const SourceTree = ({ sourceData, brain }) => {
   const [codeState, setCodeState] = useState(brain.getState().code)
   const { file = '', version } = codeState || {}
 
-  const [expanded, setExpanded] = useState({})
+  const [expanded, setExpanded] = useState(
+    file
+      .split('/')
+      .reduce((memo, p, i) => memo.concat(i ? memo[i - 1] + '/' + p : p), [])
+      .reduce((memo, p) => ({ ...memo, [p]: true }), {})
+  )
 
   useEffect(() => {
     const fn = brainState => setCodeState(brainState.code)
@@ -38,11 +43,12 @@ const SourceTree = ({ sourceData, brain }) => {
           <StateIcon
             state={sourceData.files[node.id].versions[version].state}
           />
-        )
+        ),
+        isSelected: node.id === file
       })
     })
     return _translate
-  }, [expanded, sourceData.files, version])
+  }, [expanded, sourceData.files, version, file])
 
   const tree = useMemo(() => sourceData.tree.map(translateNode), [
     sourceData.tree,
